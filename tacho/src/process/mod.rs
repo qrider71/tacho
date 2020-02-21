b975350be1fda0e3e4faa04e64ad0b0aa51c8c13
Markus Reith <mreith@gmx.net>
2020-02-21 11:28:45 +0100

    refactoring

--- tacho/src/process/mod.rs
+++ tacho/src/process/mod.rs
@@ -1,37 +1,65 @@
-use std::process::{Command, Output};
-use std::io::{self, Write};
+use std::process::Command;
 use std::time::Instant;
 
 mod options;
 
-pub fn run_process(cmd_name: &str, args: Vec<&str>) -> Result<(u128, Output), std::io::Error> {
+pub enum TachoOutput {
+    NoOutput,
+    FullOutput(String),
+}
+
+pub struct TachoResult {
+    duration: u128,
+    output: TachoOutput,
+}
+
+pub fn run_process(
+    cmd_name: &str,
+    args: Vec<&str>,
+    tacho_options: &options::TachoOptions,
+) -> Result<TachoResult, std::io::Error> {
     let start = Instant::now();
     let output = Command::new(cmd_name).args(args).output();
     let duration = start.elapsed().as_millis();
 
     return match output {
-        Ok(out) => Ok((duration, out)),
+        Ok(out) => Ok(TachoResult {
+            duration: duration,
+            output: if tacho_options.quiet {
+                TachoOutput::NoOutput
+            } else {
+                TachoOutput::FullOutput(String::from_utf8(out.stdout).unwrap())
+            },
+        }),
         Err(e) => Err(e),
     };
 }
 
 pub fn run_command(args: Vec<&str>) -> Result<(), String> {
     let command_line = options::get_command_line(args);
-
     match command_line {
         Ok((cmd, params, tacho_options)) => {
-            let output_result = run_process(cmd, params);
-            match output_result {
-                Ok((duration, output)) => {
-                    if !tacho_options.quiet {
-                        io::stdout().write_all(&output.stdout).unwrap();
-                    }
-                    println!("[ Tacho: {} duration[{}ms] ]", tacho_options.tag, duration);
-                    return Ok(());
-                }
+            let run_result = run_process(cmd, params, &tacho_options);
+            match run_result {
+                Ok(result) => return process_output(result, &tacho_options),
                 Err(e) => return Err(e.to_string()),
             };
-        },
-        Err(s) => return Err(s)
+        }
+        Err(s) => return Err(s),
     }
+}
+
+fn process_output(
+    result: TachoResult,
+    tacho_options: &options::TachoOptions,
+) -> Result<(), String> {
+    match result.output {
+        TachoOutput::FullOutput(s) => println!("{}", s),
+        TachoOutput::NoOutput => (),
+    }
+    println!(
+        "[ Tacho: {} duration[{}ms] ]",
+        tacho_options.tag, result.duration
+    );
+    return Ok(());
 }
