087192299eb02e99d6fcba6bdef24e23f780a584
Markus Reith <mreith@gmx.net>
2020-03-06 16:53:54 +0100

    update test script

--- tacho/test/test.sh
+++ tacho/test/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo just a test for Tacho, just sleeping [900000ns] test [999.123ss] test2 [10,123 ms] 123s!
+echo just a test [900000ns] test [999.123ss] test2 [10,123 ms] 123s!
+echo test2 [0,123 s] 123s!
 sleep 1
